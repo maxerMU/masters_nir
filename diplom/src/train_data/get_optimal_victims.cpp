0fb00da941cc80dcf0a7fe65c5db3367a3dab6a6
Mitsevich Maxim <max_mitsevich@mail.ru>
2025-01-29 20:07:44 +0300

    clear input and victim rates

--- diplom/src/train_data/get_optimal_victims.cpp
+++ diplom/src/train_data/get_optimal_victims.cpp
@@ -5,10 +5,13 @@
 #include <string>
 #include <string_view>
 #include <ranges>
+#include <list>
 
 constexpr std::string_view INPUT_FILE_NAME = "acc_logfile2_pages";
 constexpr std::string_view RESULT_FILE_NAME = "acc_logfile2_victims";
 constexpr size_t BUF_SIZE = 32;
+constexpr size_t POSSIBLE_VICTIMS = 5;
+using victims_t = std::list<std::pair<size_t, size_t>>;
 
 std::vector<size_t> GetPages(std::string_view filename)
 {
@@ -24,13 +27,12 @@ std::vector<size_t> GetPages(std::string_view filename)
     return res;
 }
 
-size_t FindOptimalVictim(const std::vector<size_t>& pages, std::vector<size_t>& buffer, size_t current_index)
+victims_t FindOptimalVictims(const std::vector<size_t>& pages, std::vector<size_t>& buffer, size_t current_index)
 {
     if (current_index == pages.size() - 1)
-        return 0;
+        return {};
 
-    size_t victim_rate = 0;
-    size_t victim_ind = 0;
+    victims_t victims;
 
     for (size_t i = 0; i < buffer.size(); ++i)
     {
@@ -41,20 +43,34 @@ size_t FindOptimalVictim(const std::vector<size_t>& pages, std::vector<size_t>& 
             if (buffer[i] == pages[j])
                 break;
         }
-        // std::cout << i << " " << rate << std::endl;
-        if (rate > victim_rate)
+
         {
-            victim_ind = i;
-            victim_rate = rate;
+            auto it = victims.begin();
+            while (it != victims.end() && it->second >= rate)
+            {
+                ++it;
+            }
+
+            if (it == victims.end() && victims.size() < POSSIBLE_VICTIMS)
+            {
+                victims.push_back({i, rate});
+            }
+            else if (it != victims.end())
+            {
+                victims.insert(it, {i, rate});
+                if (victims.size() > POSSIBLE_VICTIMS)
+                    victims.pop_back();
+            }
+            
         }
     }
 
-    return victim_ind;
+    return victims;
 }
 
-std::vector<size_t> GetVictims(const std::vector<size_t>& pages)
+std::vector<victims_t> GetVictims(const std::vector<size_t>& pages)
 {
-    std::vector<size_t> victims;
+    std::vector<victims_t> victims;
     std::vector<size_t> buffer;
 
     for (size_t i = 0; i < pages.size(); ++i)
@@ -65,18 +81,18 @@ std::vector<size_t> GetVictims(const std::vector<size_t>& pages)
         if (std::ranges::find(buffer, page) != buffer.end())
         {
             // already in buffer
-            victims.push_back(BUF_SIZE);
+            victims.push_back({});
         }
         else if (buffer.size() < BUF_SIZE)
         {
-            victims.push_back(buffer.size());
+            victims.push_back({{buffer.size(), 1}});
             buffer.push_back(page);
         }
         else
         {
-            size_t victim = FindOptimalVictim(pages, buffer, i);
-            buffer[victim] = page;
-            victims.push_back(victim);
+            victims_t victims_rates = FindOptimalVictims(pages, buffer, i);
+            buffer[victims_rates.front().first] = page;
+            victims.push_back(victims_rates);
             // std::cout << "victim: " << victim << std::endl;
         }
     }
@@ -84,11 +100,15 @@ std::vector<size_t> GetVictims(const std::vector<size_t>& pages)
     return victims;
 }
 
-void SaveVictims(const std::vector<size_t>& victims, std::string_view filename)
+void SaveVictims(const std::vector<victims_t>& victims, std::string_view filename)
 {
     std::ofstream file(filename.data());
-    for (size_t victim : victims)
-        file << victim << std::endl;
+    for (victims_t victims_rates : victims)
+    {
+        file << victims_rates.size() << std::endl;
+        for (const auto victim_rate : victims_rates)
+            file << victim_rate.first << " " << victim_rate.second << std::endl;
+    }
 }
 
 int main(void)
@@ -99,7 +119,7 @@ int main(void)
     // input.resize(1000);
     std::cout << input.size() << std::endl;
 
-    std::vector<size_t> victims = GetVictims(input);
+    std::vector<victims_t> victims = GetVictims(input);
     std::cout << victims.size() << std::endl;
     SaveVictims(victims, RESULT_FILE_NAME);
 
