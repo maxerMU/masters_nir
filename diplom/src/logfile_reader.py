7f39199cbf70b72a578576764e4938e327f838a4
Mitsevich Maxim <max_mitsevich@mail.ru>
2025-03-09 17:59:04 +0300

    page rel id

--- diplom/src/logfile_reader.py
+++ diplom/src/logfile_reader.py
@@ -3,7 +3,8 @@ import re
 
 @dataclass
 class Page:
-    buffer: int
+    # buffer: int
+    rel_id: int
     # is_local_temp: int
     fork_num: int
     block_num: int
@@ -18,33 +19,38 @@ class Page:
     relminmxid: int
     hit: int 
 
+    def get_page_id(self) -> str:
+        return f"{self.rel_id} {self.fork_num} {self.block_num}"
+
 def read_pages(filepath: str):
     with open(filepath, "r") as file:
         data = file.read()
 
-    pattern = r"buffer={(\d+)} is_local_temp={(\w+)} fork_num={(\w+)} block_num={(\d+)} mode={(\w+)} strategy={} relam={(\d+)} relfilenode={(\d+)} relhasindex={(\w+)} relpersistence={(\w+)} relkind={(\w+)} relnatts={(\d+)} relfrozenxid={(\d+)} relminmxid={(\d+)} hit={(\w+)}"
+    pattern = r"buffer={(\d+)} rel_id={(\d+)} is_local_temp={(\w+)} fork_num={(\w+)} block_num={(\d+)} mode={(\w+)} strategy={} relam={(\d+)} relfilenode={(\d+)} relhasindex={(\w+)} relpersistence={(\w+)} relkind={(\w+)} relnatts={(\d+)} relfrozenxid={(\d+)} relminmxid={(\d+)} hit={(\w+)}"
     matches = re.findall(pattern, data)
 
     pages = []
 
     for match in matches:
         buffer = int(match[0])
-        is_local_temp = 1 if match[1] == "true" else 0
-        fork_num = ["MAIN_FORKNUM", "FSM_FORKNUM", "VISIBILITYMAP_FORKNUM", "INIT_FORKNUM"].index(match[2])
-        block_num = int(match[3])
-        mode = ["RBM_NORMAL", "RBM_ZERO_AND_LOCK", "RBM_ZERO_AND_CLEANUP_LOCK", "RBM_ZERO_ON_ERROR", "RBM_NORMAL_NO_LOG"].index(match[4])
-        relam = int(match[5])
-        relfilenode = int(match[6])
-        relhasindex = 1 if match[7] == "true" else 0
-        relpersistence = ["p", "u", "t"].index(match[8])
-        relkind = ["r", "i", "S", "t", "v", "m", "c", "f", "p", "I"].index(match[9])
-        relnatts = int(match[10])
-        relfrozenxid = int(match[11])
-        relminmxid = int(match[12])
-        hit = 1 if match[13] == "true" else 0
+        rel_id = int(match[1])
+        is_local_temp = 1 if match[2] == "true" else 0
+        fork_num = ["MAIN_FORKNUM", "FSM_FORKNUM", "VISIBILITYMAP_FORKNUM", "INIT_FORKNUM"].index(match[3])
+        block_num = int(match[4])
+        mode = ["RBM_NORMAL", "RBM_ZERO_AND_LOCK", "RBM_ZERO_AND_CLEANUP_LOCK", "RBM_ZERO_ON_ERROR", "RBM_NORMAL_NO_LOG"].index(match[5])
+        relam = int(match[6])
+        relfilenode = int(match[7])
+        relhasindex = 1 if match[8] == "true" else 0
+        relpersistence = ["p", "u", "t"].index(match[9])
+        relkind = ["r", "i", "S", "t", "v", "m", "c", "f", "p", "I"].index(match[10])
+        relnatts = int(match[11])
+        relfrozenxid = int(match[12])
+        relminmxid = int(match[13])
+        hit = 1 if match[14] == "true" else 0
 
         page = Page(
-            buffer,
+            # buffer,
+            rel_id,
             # is_local_temp, 
             fork_num,
             block_num,
@@ -60,13 +66,13 @@ def read_pages(filepath: str):
             hit,
         )
         pages.append(page)
-    
+
     return pages
 
 def save_pages_accs(filepath: str, pages: list[Page]):
     with open(filepath, "w") as f:
         for page in pages:
-            f.write(f"{page.buffer}\n")
+            f.write(f"{page.get_page_id()}\n")
 
 def read_optimal_results(filepath: str):
     optimal_results = []
--- diplom/src/train_data/get_optimal_victims.cpp
+++ diplom/src/train_data/get_optimal_victims.cpp
@@ -1,100 +1,113 @@
 #include <iostream>
 #include <fstream>
 
+#include <algorithm>
 #include <vector>
 #include <string>
 #include <string_view>
 #include <ranges>
 #include <list>
 
-constexpr std::string_view INPUT_FILE_NAME = "acc_logfile2_pages";
-constexpr std::string_view RESULT_FILE_NAME = "acc_logfile2_victims";
-constexpr size_t BUF_SIZE = 32;
 constexpr size_t POSSIBLE_VICTIMS = 5;
 using victims_t = std::list<std::pair<size_t, size_t>>;
 
-std::vector<size_t> GetPages(std::string_view filename)
+// using Page = std::string;
+struct Page
 {
-    std::vector<size_t> res;
-    size_t buf;
+    size_t relId;
+    size_t forkNum;
+    size_t blockNum;
+
+    bool operator==(const Page& rhs) const = default;
+};
+
+std::istream &operator>>(std::istream &is, Page& page)
+{
+    is >> page.relId >> page.forkNum >> page.blockNum;
+    return is;
+}
+
+std::vector<Page> GetPages(std::string_view filename)
+{
+    std::vector<Page> res;
     std::ifstream file(filename.data());
 
-    while( file >> buf )
+    Page page;
+    while( file >> page )
     {
-        res.push_back(buf);
+        res.push_back(page);
     }
 
     return res;
 }
 
-victims_t FindOptimalVictims(const std::vector<size_t>& pages, std::vector<size_t>& buffer, size_t current_index)
+size_t GetDistanceToNextAcc(const std::vector<Page>& pages, const Page& page, size_t currentIndex, size_t maxRate)
 {
-    if (current_index == pages.size() - 1)
-        return {};
+    size_t rate = 0;
+    for (size_t i = currentIndex + 1; i < pages.size() && rate <= maxRate; ++i)
+    {
+        rate++;
+        if (pages[i] == page)
+            break;
+    }
 
-    victims_t victims;
+    return rate;
+}
+
+void UpdateRates(std::vector<size_t>& rates, const std::vector<Page>& pages, const std::vector<Page>& buffer, size_t currentIndex)
+{
+    for (size_t i = 0; i < buffer.size(); ++i)
+    {
+        if (rates[i] != 0)
+            rates[i] -= 1;
+    }
 
     for (size_t i = 0; i < buffer.size(); ++i)
     {
-        size_t rate = 0;
-        for (size_t j = current_index; j < pages.size(); ++j)
+        if (rates[i] == 0)
         {
-            rate++;
-            if (buffer[i] == pages[j])
-                break;
-        }
-
-        {
-            auto it = victims.begin();
-            while (it != victims.end() && it->second >= rate)
-            {
-                ++it;
-            }
-
-            if (it == victims.end() && victims.size() < POSSIBLE_VICTIMS)
-            {
-                victims.push_back({i, rate});
-            }
-            else if (it != victims.end())
-            {
-                victims.insert(it, {i, rate});
-                if (victims.size() > POSSIBLE_VICTIMS)
-                    victims.pop_back();
-            }
-            
+            // optimization by max rate
+            bool isLastZero = std::any_of(rates.begin() + i + 1, rates.begin() + buffer.size(), [](size_t el) {return el == 0;});
+            size_t maxRate = isLastZero ? std::ranges::max(rates) : pages.size();
+            rates[i] = GetDistanceToNextAcc(pages, buffer[i], currentIndex, maxRate);
         }
     }
-
-    return victims;
 }
 
-std::vector<victims_t> GetVictims(const std::vector<size_t>& pages)
+std::vector<victims_t> GetVictims(const std::vector<Page>& pages, size_t bufferSize)
 {
     std::vector<victims_t> victims;
-    std::vector<size_t> buffer;
+    std::vector<Page> buffer;
+    std::vector<size_t> rates(bufferSize, 0);
 
     for (size_t i = 0; i < pages.size(); ++i)
     {
+        const Page& page = pages[i];
+
         if (i % 10000 == 0)
             std::cout << i << std::endl;
-        size_t page = pages[i];
+
         if (std::ranges::find(buffer, page) != buffer.end())
         {
             // already in buffer
             victims.push_back({});
         }
-        else if (buffer.size() < BUF_SIZE)
+        else if (buffer.size() < bufferSize)
         {
             victims.push_back({{buffer.size(), 1}});
             buffer.push_back(page);
         }
         else
         {
-            victims_t victims_rates = FindOptimalVictims(pages, buffer, i);
-            buffer[victims_rates.front().first] = page;
-            victims.push_back(victims_rates);
+            auto maxRateIt = std::ranges::max_element(rates);
+            size_t victim = std::ranges::distance(rates.begin(), maxRateIt);
+            buffer[victim] = page;
+            victims.push_back({{victim, 1}});
+            rates[victim] = 0;
             // std::cout << "victim: " << victim << std::endl;
         }
+
+        UpdateRates(rates, pages, buffer, i);
     }
 
     return victims;
@@ -106,22 +119,29 @@ void SaveVictims(const std::vector<victims_t>& victims, std::string_view filenam
     for (victims_t victims_rates : victims)
     {
         file << victims_rates.size() << std::endl;
-        for (const auto victim_rate : victims_rates)
+        for (const auto& victim_rate : victims_rates)
             file << victim_rate.first << " " << victim_rate.second << std::endl;
     }
 }
 
-int main(void)
+int main(int argc, char** argv)
 {
-    std::vector<size_t> input = GetPages(INPUT_FILE_NAME);
-    std::cout << input.size() << std::endl;
+    if (argc != 4)
+    {
+        std::cout << "Excpect arguments: <buf_size> <input file> <res file>" << std::endl;
+        return 1;
+    }
+    std::cout << std::string(argv[1]) << std::endl;
 
-    // input.resize(1000);
-    std::cout << input.size() << std::endl;
+    size_t bufSize = std::stoull(argv[1]);
+    std::string inputFile(argv[2]);
+    std::string outputFile(argv[3]);
 
-    std::vector<victims_t> victims = GetVictims(input);
+    std::vector<Page> pages = GetPages(inputFile);
+
+    std::vector<victims_t> victims = GetVictims(pages, bufSize);
     std::cout << victims.size() << std::endl;
-    SaveVictims(victims, RESULT_FILE_NAME);
+    SaveVictims(victims, outputFile);
 
     return 0;
 }
